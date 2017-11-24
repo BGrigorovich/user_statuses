bb18300dd241303c207bcbb54c20f33539c61278
bhryhorovych <bohdan.hryhorovych@gmail.com>
2017-11-24 12:40:04 +0200

    Add babel

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,15 +1,22 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'src');
 
-var config = {
-  entry: APP_DIR + '/js/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  }
+module.exports = {
+    entry: APP_DIR + '/js/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel-loader'
+            }
+        ]
+    }
 };
-
-module.exports = config;
